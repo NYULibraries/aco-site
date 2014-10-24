5923209ff22fbecfe0557d48f3afb752ed6e09f9
Laura Henze <laurahenze@gmail.com>
2014-10-24 12:06:26 -0400

    Updating SASS and compass

--- source/config.rb
+++ source/config.rb
@@ -1,4 +1,3 @@
-#require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 
@@ -10,8 +9,8 @@ images_dir = "images"
 javascripts_dir = "../build/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
+ #output_style = :expanded or :nested or :compact or :compressed
+ #output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
