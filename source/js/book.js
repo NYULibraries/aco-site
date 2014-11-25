81754cdd8ed77f08751ff42081c47a1eeac96761
Laura Henze <laurahenze@gmail.com>
2014-11-25 16:26:23 -0500

    offset for loader height should not be hardcoded

--- source/js/book.js
+++ source/js/book.js
@@ -1,133 +1,135 @@
 YUI().use(
-    'node'
-  , 'anim'
-  , 'crossframe'
-  , 'router'
-  , 'event-resize'
-  , function (Y) {
-  
+  'node', 'anim', 'crossframe', 'router', 'event-resize',
+  function(Y) {
+
     'use strict';
-    
-    var body = Y.one('body')
-      , widget = Y.one('.widget.book')
-      , appRoot = body.getAttribute("data-appRoot")
-      
+
+    var body = Y.one('body'),
+      widget = Y.one('.widget.book'),
+      appRoot = body.getAttribute("data-appRoot");
+
     body.addClass('io-loading');
-    
+
     function calculateAvailableHeight() {
-      
-	    var siblings = widget.siblings()
-           , viewport = Y.DOM.viewportRegion()
-           , availableHeight = viewport.height;
-      
-        siblings.each( function( node ) {
-            availableHeight = availableHeight - node.get('offsetHeight');
-        });
-        
-        if ( body.hasClass('io-loading') )  {
-            availableHeight = availableHeight + 50;
-        }
-        
-        return availableHeight;
+
+      var siblings = widget.siblings(),
+        viewport = Y.DOM.viewportRegion(),
+        availableHeight = viewport.height;
+      // Push the iframe down 5px to make up for the 5 pixels 
+      // space created by the curved corners of the browser?
+      // Not elegant but to consider. 
+     // availableHeight += 5;
+      siblings.each(function(node) {
+        availableHeight = availableHeight - node.get('offsetHeight');
+      });
+
+      if (body.hasClass('io-loading')) {
+        var loadingAnimation = Y.one('.bubblingG'),
+          loaderHeight = loadingAnimation.get('offsetHeight');
+        availableHeight = availableHeight + loaderHeight;
+      }
+
+      return availableHeight;
     }
-    
+
     function resizeBookView() {
-        widget.setStyles({ height : calculateAvailableHeight() });
-    }    
-      
+      widget.setStyles({
+        height: calculateAvailableHeight()
+      });
+    }
+
     function hideSiblings() {
-    
-        widget.siblings().each(function(node) {
-            node.addClass('hiddenSiblings');
-        });
-        
-        resizeBookView();
+
+      widget.siblings().each(function(node) {
+        node.addClass('hiddenSiblings');
+      });
+
+      resizeBookView();
     }
-      
+
     function showSiblings() {
-    
-        widget.siblings().each(function(node) {
-            node.removeClass('hiddenSiblings');
-        });
-        
-        resizeBookView();
-    }   
-    
-    function requestReplaceLoadBook ( request ) {
 
-        var src = widget.getAttribute('data-sourceUrl')
-          , bookTheme = widget.getAttribute('data-bookTheme')
-          , identifier = request.params.identifier
-          , page = ( request.params.page ) ? request.params.page : 1;          
-                    
-        widget.setAttribute('data-identifier', identifier);
-        
-        if ( request.src  === 'replace' ) {
-                        
-            src = src + '/books/' + request.params.identifier + '/' + page;
-                            
-            if ( bookTheme ) {
-                src = src + '?ctheme=' + bookTheme;
-            }
+      widget.siblings().each(function(node) {
+        node.removeClass('hiddenSiblings');
+      });
 
-            widget.set('src', src);  
-                        
-        }
-         
+      resizeBookView();
     }
-    
+
+    function requestReplaceLoadBook(request) {
+
+      var src = widget.getAttribute('data-sourceUrl'),
+        bookTheme = widget.getAttribute('data-bookTheme'),
+        identifier = request.params.identifier,
+        page = (request.params.page) ? request.params.page : 1;
+
+      widget.setAttribute('data-identifier', identifier);
+
+      if (request.src === 'replace') {
+
+        src = src + '/books/' + request.params.identifier + '/' + page;
+
+        if (bookTheme) {
+          src = src + '?ctheme=' + bookTheme;
+        }
+
+        widget.set('src', src);
+
+      }
+
+    }
+
     var router = new Y.Router({
-        root : appRoot,
-        routes: [
-            {
-                path : '/book/:identifier/:page', 
-                callbacks : requestReplaceLoadBook
-            },
-            {
-                path : '/book/:identifier', 
-                callbacks : requestReplaceLoadBook
-            }
-        ]
+      root: appRoot,
+      routes: [{
+        path: '/book/:identifier/:page',
+        callbacks: requestReplaceLoadBook
+      }, {
+        path: '/book/:identifier',
+        callbacks: requestReplaceLoadBook
+      }]
     });
 
     Y.on('windowresize', resizeBookView);
-    
+
     Y.on('button:button-fullscreen:on', hideSiblings);
 
     Y.on('button:button-fullscreen:off', showSiblings);
-    
-    Y.on('openlayers:change', function ( data ) {
-        router.save( '/book/' + widget.getAttribute('data-identifier') + '/' + data.sequence );
+
+    Y.on('openlayers:change', function(data) {
+      router.save('/book/' + widget.getAttribute('data-identifier') + '/' + data.sequence);
     });
 
-    Y.Global.on("crossframe:message", function ( o, data ) {
+    Y.Global.on("crossframe:message", function(o, data) {
 
-        var message = JSON.parse(data.message);
-             
-        Y.fire( message.fire, message.data );
-            
+      var message = JSON.parse(data.message);
+
+      Y.fire(message.fire, message.data);
+
     });
-    
-    widget.on('load', function( ) {
 
-        var anim = new Y.Anim({
-                node : this,
-                to : { opacity : 1 },
-                duration : 0.3
-        });
+    widget.on('load', function() {
 
-        resizeBookView();
-            
-        anim.run();
+      var anim = new Y.Anim({
+        node: this,
+        to: {
+          opacity: 1
+        },
+        duration: 0.3
+      });
 
-        body.removeClass('io-loading');
-    
+      resizeBookView();
+
+      anim.run();
+
+      body.removeClass('io-loading');
+
     });
-    
+
     resizeBookView();
-    
-    
+
+
     // initial request
-    router.replace( router.getPath() );
-    
-});
+    router.replace(router.getPath());
+
+  });
