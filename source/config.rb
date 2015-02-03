a8124308638e5c53c9b731f59cb2a8851e045c9f
Laura Henze <laurahenze@gmail.com>
2015-02-02 19:33:15 -0500

    Prevents build errors

--- source/config.rb
+++ source/config.rb
@@ -4,7 +4,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "../build/css"
-sass_dir = "sass"
+sass_dir = "./sass"
 images_dir = "images"
 javascripts_dir = "../build/js"
 
