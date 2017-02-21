540b329c7ccb1b2c0b5aa0ffc7c782becf7b1c59
Laura Henze <laura.henze@nyu.edu>
2017-02-21 13:28:46 -0500

    Search landing page - if nothing put in search page

--- source/js/search_form_adv.js
+++ source/js/search_form_adv.js
@@ -25,7 +25,12 @@ YUI().use("node", 'anim', "event", function(Y) {
         var destinationString = whichField1 + "=" + value1;
 
         Y.log("Zounds! DestinationString is " + destinationString);
-        location.href = currentTarget.get("action") + "?" + destinationString;
+        Y.log("Zounds! value1 is " + value1);
+        if (value1 !== "") {
+            location.href = currentTarget.get("action") + "?" + destinationString;
+        } else {
+            location.href = "/aco/searchcollections/";
+        }
 
     }
 
