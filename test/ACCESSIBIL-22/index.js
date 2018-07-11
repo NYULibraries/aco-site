fcc03c79323457df4dc2f92e8c57389e0e2f9e96
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-07-11 14:06:49 -0400

    fix test to conform to new request

--- test/ACCESSIBIL-22/index.js
+++ test/ACCESSIBIL-22/index.js
@@ -7,7 +7,7 @@ module.exports = {
       browser
         .url(path.join(process.env.APP_URL, 'book/princeton_aco000380/3'))
         .waitForElementVisible('body', 2000)
-        .assert.title('ʻUṣārat qalb')
+        .assert.title('ʻUṣārat qalb: Arabic Collections Online')
         .saveScreenshot(conf.imgpath(browser) + 'ACCESSIBIL-22.png')
         .end();
   }
