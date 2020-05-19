1f941c539983aacd280ac4b64318754f5d622644
Alberto Ortiz Flores <aortiz@nyu.edu>
2020-05-18 21:20:22 -0400

    add fn humanFileSize (we need to let go this codebase)

--- source/js/items.js
+++ source/js/items.js
@@ -27,9 +27,27 @@ YUI().use(
       	    return window.getSlug ( this.label ) ;
         }
 
+        // Taken from https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string#14919494
+        function humanFileSize(bytes, si) {
+          var thresh = si ? 1000 : 1024;
+          if(Math.abs(bytes) < thresh) {
+            return bytes + ' B';
+          }
+          var units = si
+          ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+          : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+          var u = -1;
+          do {
+            bytes /= thresh;
+            ++u;
+          } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+          return bytes.toFixed(1) + ' ' + units[u];
+        }
+
        return { 
            json : json ,
-           speakingurl : speakingurl
+           speakingurl : speakingurl,
+           humanFileSize: humanFileSize
        } ;
 
     }
--- source/js/search.js
+++ source/js/search.js
@@ -108,9 +108,28 @@ YUI().use(
                     return defaultvalue;
                 }
             }
+
+            // Taken from https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string#14919494
+            function humanFileSize(bytes, si) {
+              var thresh = si ? 1000 : 1024;
+              if(Math.abs(bytes) < thresh) {
+                return bytes + ' B';
+              }
+              var units = si
+              ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+              : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+              var u = -1;
+              do {
+                bytes /= thresh;
+                ++u;
+              } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+              return bytes.toFixed(1) + ' ' + units[u];
+            }
+
             return {
                 ifempty: ifempty,
                 json: json,
+                humanFileSize: humanFileSize
             };
         }
 
