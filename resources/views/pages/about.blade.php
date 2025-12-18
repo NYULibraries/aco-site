eada55a8c23f081acae518be8332a1c59a8419d4
Damon Chu <dc3800@nyu.edu>
2025-12-18 12:07:14 -0500

    added formatter, fixed trailing slash, added hamburger menu

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,22 +5,23 @@
 @section('body_class', $body_class)
 
 @section('content')
-  <main class="main container-fluid" role="main">
+    <main class="main container-fluid" role="main">
 
-    @include('partials.pagetitle')
+        @include('partials.pagetitle')
 
-    @foreach ($content['about'] as $langCode => $data)
-      <div class="col {{ $data['language']['class'] }}" lang="{{ $langCode }}" dir="{{ $data['language']['dir'] }}">
-        <h3>{{ $data['label'] }}</h3>
-        {!! $data['body'] !!}
-      </div>
-    @endforeach
+        @foreach ($content['about'] as $langCode => $data)
+            <div class="col {{ $data['language']['class'] }}" lang="{{ $langCode }}" dir="{{ $data['language']['dir'] }}">
+                <h3>{{ $data['label'] }}</h3>
+                {!! $data['body'] !!}
+            </div>
+        @endforeach
 
-    @foreach ($content['main'] as $index => $data)
-      <div class="col {{ $data['language']['class'] }}" lang="{{ $data['language']['code'] }}" dir="{{ $data['language']['dir'] }}">
-        {!! $data['html'] !!}
-      </div>
-    @endforeach
+        @foreach ($content['main'] as $index => $data)
+            <div class="col {{ $data['language']['class'] }}" lang="{{ $data['language']['code'] }}"
+                dir="{{ $data['language']['dir'] }}">
+                {!! $data['html'] !!}
+            </div>
+        @endforeach
 
-  </main>
+    </main>
 @endsection
