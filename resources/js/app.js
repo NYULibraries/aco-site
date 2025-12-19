5716e01fbe7058f655f70ba7828963eb0b778e16
Damon Chu <dc3800@nyu.edu>
2025-12-19 16:31:32 -0500

    edits for hamburger

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-	const button = document.querySelector('button');
-	const navbar = document.querySelector('.navbar-collapse');
-	let collapsed = true;
-	if (button && navbar) {
-		button.addEventListener('click', (event) => {
-			event.preventDefault();
-			collapsed ? (navbar.style.height = '283px') : (navbar.style.height = '0px');
-			collapsed = !collapsed;
-		});
-	}
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector('button[data-name="navButton"]');
+  const navbar = document.querySelector(".navbar-collapse");
+  if (button && navbar) {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      const isCollapsed = navbar.dataset.collapsed === "collapse";
+      if (isCollapsed) {
+        navbar.dataset.collapsed = "expand";
+        navbar.style.height = "283px";
+      } else {
+        navbar.dataset.collapsed = "collapse";
+        navbar.style.height = "0px";
+      }
+    });
+  }
 });
-console.log('app.js loaded');
