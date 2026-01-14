001dd7f2cd6d398f51a502f2caed2d073965b679
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-01-14 09:13:18 -0800

    Add artisan command to trigger solr query search

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Services\SolrService;
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+  $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/**
+ * artisan command used to trigger the solr service without testing or calling the whole app
+ */
+Artisan::command('solr:search {query="*:*"} {scopeIs="matches"} {sortBy="asc"}', function (SolrService $solrService, string $query, string $scopeIs, string $sortBy) {
+  $results = $solrService->search2($scopeIs, $sortBy, $query);
+
+  dump($results);
+  // $this->table(['Results'], $results);
+  // dump($query);
+})->purpose('Run basic search through the search service');
