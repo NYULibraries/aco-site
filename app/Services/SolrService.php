7a6c670e7797b215ade4baa0878e7fd86ce2b4e4
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-01-26 09:02:46 -0800

    Add data transformation

--- app/Services/SolrService.php
+++ app/Services/SolrService.php
@@ -22,16 +22,16 @@ class SolrService
    */
   public function buildQuery(
     string $fieldSelect = 'q', // what field to search on
-string $searchString,  // what query to use
-string $scopeIs = 'matches', // how to match results
-string $sortField = 'score', // how to order results
-string $sortDir = 'desc',
-$rowStart,
-$rows
+    string $searchString,  // what query to use
+    string $scopeIs = 'matches', // how to match results
+    string $sortField = 'score', // how to order results
+    string $sortDir = 'desc',
+    $rowStart,
+    $rows
   ): Query
   // public function buildQuery(string $searchString, array $options = []): Query
   {
-/**
+    /**
      * Defaults
      */
     // $defaults = [
@@ -201,9 +201,9 @@ $rows
     $sanitizedSearchString = $helper->escapeTerm($searchString);
     if ($fieldSelect == 'q') {
       if ($scopeIs == 'matches') {
-$finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
-    } else {
-$finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
+        $finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
+      } else {
+        $finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
       }
     } else {
       echo "<<<< this is the else \n";
@@ -211,14 +211,14 @@ $finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitized
       // only adds the query into the fq otions and not the q options
 
     }
-echo "finalQuery before sanitizing: \n";
+    echo "finalQuery before sanitizing: \n";
     echo "$finalQuery \n";
     $query->setQuery($finalQuery);
 
     /**
      * FILTER QUERY fq
      */
-        // these always have to happen as of 2026
+    // these always have to happen as of 2026
     $query->createFilterQuery(md5('bundle:dlts_book'))->setQuery('bundle:dlts_book');
     $query->createFilterQuery(md5('sm_collection_code:aco'))->setQuery('sm_collection_code:aco');
     $query->createFilterQuery(md5('ss_language:en'))->setQuery('ss_language:en');
@@ -236,12 +236,12 @@ echo "finalQuery before sanitizing: \n";
      * TODO: sanitize query
      * TODO:
      */
-//     $helper = $query->getHelper();
-//     $sanitizedSearchString = $helper->escapeTerm($searchString);
-//     $finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
-//     echo "finalQuery before sanitizing: \n";
-//     echo "$finalQuery \n";
-    //     $query->setQuery($finalQuery);
+    // $helper = $query->getHelper();
+    // $sanitizedSearchString = $helper->escapeTerm($searchString);
+    // $finalQuery = "((content_und:$sanitizedSearchString OR content_und_ws:$sanitizedSearchString OR content_en:$sanitizedSearchString OR content:$sanitizedSearchString))";
+    // echo "finalQuery before sanitizing: \n";
+    // echo "$finalQuery \n";
+    // $query->setQuery($finalQuery);
     // if (!empty($options['q'])) {
     //   $q = trim($options['q']);
     //   if ($scopeIs === 'matches') {
@@ -666,11 +666,12 @@ echo "finalQuery before sanitizing: \n";
    * @param int $rowStart
    * @param int $rows
    */
-  public function search2(string $query = '*', string $scopeIs = 'matches', string $sortField, string $sortDir, $rowStart, $rows): array
+  public function search2(string $fieldSelect, string $query = '*', string $scopeIs = 'matches', string $sortField, string $sortDir, $rowStart, $rows): array
   // public function search2($query): array
   {
-    echo "running serach2 \n";
+    echo "running search2 \n";
     echo "-----args-----\n";
+    echo "fieldSelect: $fieldSelect \n";
     echo "query: $query \n";
     echo "scopeIs: $scopeIs \n";
     echo "sortField: $sortField \n";
@@ -680,245 +681,218 @@ echo "finalQuery before sanitizing: \n";
     echo "rows: $rows \n";
     echo "-----args-----\n";
 
-    $BuiltQuery = $this->buildQuery($query, $scopeIs, $sortField, $sortDir, $rowStart, $rows);
-    $compareQuery = "select?wt=json&fl=*&fq=bundle:dlts_book&fq=sm_collection_code:aco&fq=ss_language:en&rows=10&start=0&sort=score%20desc&q=((content_und:arabs%20OR%20content_und_ws:arabs%20OR%20content_en:arabs%20OR%20content:arabs))";
+    $BuiltQuery = $this->buildQuery(
+      fieldSelect: $fieldSelect,
+      searchString: $query,
+      scopeIs: $scopeIs,
+      sortField: $sortField,
+      sortDir: $sortDir,
+      rowStart: $rowStart,
+      rows: $rows
+    );
+    // $compareQuery = "select?wt=json&fl=*&fq=bundle:dlts_book&fq=sm_collection_code:aco&fq=ss_language:en&rows=10&start=0&sort=score%20desc&q=((content_und:arabs%20OR%20content_und_ws:arabs%20OR%20content_en:arabs%20OR%20content:arabs))";
     // $comparison = compareQueryToUrl($BuiltQuery, $query, $compareQuery)
     // // THIS IS WHERE THE QUERY IS EXECUTED
-    // dump($BuiltQuery);
-    // $resultset = $this->solrClient->select($query);
+    dump($BuiltQuery);
+    $resultset = $this->solrClient->select($BuiltQuery);
+    // dump($resultset);
 
     // // 7. extract more data form the request
     // // returns documents and facets
-    // $total = $resultset->getNumFound();
+    $total = $resultset->getNumFound();
 
+    // result sets are already iterable, don't convert solarium call to iterator
     // $docs = iterator_to_array($resultset);
 
-    // $documents = [];
+    // catcher
+    $documents = [];
 
+    // TODO: figure out if this should be it's own method
     // // 8. data transformation
-    // foreach ($docs as $doc) {
+    foreach ($resultset as $doc) {
 
-    //   $publocation = [];
+      $publocation = [];
+      if (isset($doc->ss_publocation)) {
+        $publocation[] = [
+          'label' => $doc->ss_publocation,
+          'path' => "search/?provider={$doc->ss_publocation}",
+        ];
+      }
 
-    //   if (isset($doc->ss_publocation)) {
-    //     $publocation[] = [
-    //       'label' => $doc->ss_publocation,
-    //       'path' => "search/?provider={$doc->ss_publocation}",
-    //     ];
-    //   }
+      $ar_publocation = [];
 
-    //   $ar_publocation = [];
+      if (isset($doc->ss_ar_publocation)) {
+        $ar_publocation[] = [
+          'label' => $doc->ss_ar_publocation,
+          'path' => "search/?provider={$doc->ss_ar_publocation}",
+        ];
+      }
 
-    //   if (isset($doc->ss_ar_publocation)) {
-    //     $ar_publocation[] = [
-    //       'label' => $doc->ss_ar_publocation,
-    //       'path' => "search/?provider={$doc->ss_ar_publocation}",
-    //     ];
-    //   }
+      $providers = [];
 
-    //   $providers = [];
+      if (isset($doc->sm_provider_label)) {
+        foreach ($doc->sm_provider_label as $provider) {
+          $providers[] = [
+            'label' => $provider,
+            'path' => "search/?provider={$provider}",
+          ];
+        }
+      }
 
-    //   if (isset($doc->sm_provider_label)) {
-    //     foreach ($doc->sm_provider_label as $provider) {
-    //       $providers[] = [
-    //         'label' => $provider,
-    //         'path' => "search/?provider={$provider}",
-    //       ];
-    //     }
-    //   }
+      $publishers = [];
 
-    //   $publishers = [];
+      if (isset($doc->sm_publisher)) {
+        foreach ($doc->sm_publisher as $publisher) {
+          $publishers[] = [
+            'label' => $publisher,
+            'path' => "search/?publisher={$publisher}",
+          ];
+        }
+      }
 
-    //   if (isset($doc->sm_publisher)) {
-    //     foreach ($doc->sm_publisher as $publisher) {
-    //       $publishers[] = [
-    //         'label' => $publisher,
-    //         'path' => "search/?publisher={$publisher}",
-    //       ];
-    //     }
-    //   }
+      $topics = [];
 
-    //   $topics = [];
+      if (isset($doc->sm_field_topic)) {
+        foreach ($doc->sm_field_topic as $topic) {
+          $topics[] = [
+            'label' => $topic,
+            'path' => "search?category={$topic}&scope=matches",
+          ];
+        }
+      }
 
-    //   if (isset($doc->sm_field_topic)) {
-    //     foreach ($doc->sm_field_topic as $topic) {
-    //       $topics[] = [
-    //         'label' => $topic,
-    //         'path' => "search?category={$topic}&scope=matches",
-    //       ];
-    //     }
-    //   }
+      $subjects = [];
 
-    //   $subjects = [];
+      if (isset($doc->zm_subject)) {
+        foreach ($doc->zm_subject as $subject) {
+          $subject = json_decode($subject);
+          $subject->path = "search?subject={$subject->name}";
+          $subjects[] = $subject;
+        }
+      }
 
-    //   if (isset($doc->zm_subject)) {
-    //     foreach ($doc->zm_subject as $subject) {
-    //       $subject = json_decode($subject);
-    //       $subject->path = "search?subject={$subject->name}";
-    //       $subjects[] = $subject;
-    //     }
-    //   }
+      $partners_map = [
+        'Arabic collections online' => 'المجموعات العربية على الانترنت',
+        'New York University Libraries' => 'مكتبات جامعة نيويورك',
+        'Princeton University Libraries' => 'مكتبات جامعة برينستون',
+        'Cornell University Libraries' => 'مكتبات جامعة كورنيل',
+        'Columbia University Libraries' => 'مكتبات جامعة كولومبيا',
+        'American University of Beirut' => 'الجامعة الاميركية في بيروت',
+        'American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
+        'The American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
+        'United Arab Emirates National Archives' => 'الامارات العربية المتحدة - الارشيف الوطني',
+      ];
 
-    //   $partners_map = [
-    //     'Arabic collections online' => 'المجموعات العربية على الانترنت',
-    //     'New York University Libraries' => 'مكتبات جامعة نيويورك',
-    //     'Princeton University Libraries' => 'مكتبات جامعة برينستون',
-    //     'Cornell University Libraries' => 'مكتبات جامعة كورنيل',
-    //     'Columbia University Libraries' => 'مكتبات جامعة كولومبيا',
-    //     'American University of Beirut' => 'الجامعة الاميركية في بيروت',
-    //     'American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
-    //     'The American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
-    //     'United Arab Emirates National Archives' => 'الامارات العربية المتحدة - الارشيف الوطني',
-    //   ];
+      $partners = [];
 
-    //   $partners = [];
+      $partners_ar = [];
 
-    //   $partners_ar = [];
+      if (isset($doc->zm_partner)) {
+        foreach ($doc->zm_partner as $partner) {
+          $partner = json_decode($partner);
+          $partner->path = "search?partner={$partner->name}";
+          $partners[] = $partner;
+          if (isset($partners_map[$partner->name])) {
+            $partners_ar[] = [
+              'label' => $partners_map[$partner->name],
+              'path' => "search?partner={$partner->name}",
+            ];
+          }
+        }
+      }
 
-    //   if (isset($doc->zm_partner)) {
-    //     foreach ($doc->zm_partner as $partner) {
-    //       $partner = json_decode($partner);
-    //       $partner->path = "search?partner={$partner->name}";
-    //       $partners[] = $partner;
-    //       if (isset($partners_map[$partner->name])) {
-    //         $partners_ar[] = [
-    //           'label' => $partners_map[$partner->name],
-    //           'path' => "search?partner={$partner->name}",
-    //         ];
-    //       }
-    //     }
-    //   }
+      $authors = [];
 
-    //   $authors = [];
+      if (isset($doc->sm_author)) {
+        foreach ($doc->sm_author as $author) {
+          $authors[] = [
+            'label' => $author,
+            'path' => "search?subject={$author}",
+          ];
+        }
+      }
 
-    //   if (isset($doc->sm_author)) {
-    //     foreach ($doc->sm_author as $author) {
-    //       $authors[] = [
-    //         'label' => $author,
-    //         'path' => "search?subject={$author}",
-    //       ];
-    //     }
-    //   }
+      $authors_ar = [];
 
-    //   $authors_ar = [];
+      if (isset($doc->sm_ar_author)) {
+        foreach ($doc->sm_ar_author as $author) {
+          $authors_ar[] = [
+            'label' => $author,
+            'path' => "search?subject={$author}",
+          ];
+        }
+      }
 
-    //   if (isset($doc->sm_ar_author)) {
-    //     foreach ($doc->sm_ar_author as $author) {
-    //       $authors_ar[] = [
-    //         'label' => $author,
-    //         'path' => "search?subject={$author}",
-    //       ];
-    //     }
-    //   }
+      $pdf_hi = [];
 
-    //   $pdf_hi = [];
+      if (isset($doc->zm_pdf_hi) && isset($doc->zm_pdf_hi[0])) {
+        $pdf_hi = json_decode($doc->zm_pdf_hi[0]);
+      }
 
-    //   if (isset($doc->zm_pdf_hi) && isset($doc->zm_pdf_hi[0])) {
-    //     $pdf_hi = json_decode($doc->zm_pdf_hi[0]);
-    //   }
+      $pdf_lo = [];
 
-    //   $pdf_lo = [];
+      if (isset($doc->zm_pdf_lo) && isset($doc->zm_pdf_lo[0])) {
+        $pdf_lo = json_decode($doc->zm_pdf_lo[0]);
+      }
 
-    //   if (isset($doc->zm_pdf_lo) && isset($doc->zm_pdf_lo[0])) {
-    //     $pdf_lo = json_decode($doc->zm_pdf_lo[0]);
-    //   }
+      $pubdate = 'n.d.';
 
-    //   $pubdate = 'n.d.';
+      if (isset($doc->ss_pubdate) && isset($doc->ss_pubdate)) {
+        $pubdate = $doc->ss_pubdate;
+      }
 
-    //   if (isset($doc->ss_pubdate) && isset($doc->ss_pubdate)) {
-    //     $pubdate = $doc->ss_pubdate;
-    //   }
+      $documents[] = [
+        'en' => [
+          'title' => $doc->ss_title_long,
+          'identifier' => $doc->ss_book_identifier,
+          'path' => "book/{$doc->ss_book_identifier}/1",
+          'noid' => $doc->ss_noid,
+          'handle' => $doc->ss_handle,
+          'manifest' => $doc->ss_manifest,
+          'subjects' => $subjects,
+          'pubdate' => $pubdate,
+          'pdf' => [
+            'hi' => $pdf_hi,
+            'lo' => $pdf_lo,
+          ],
+          'authors' => $authors,
+          'partners' => $partners,
+          'topics' => $topics,
+          'publishers' => $publishers,
+          'provider' => $providers,
+          'publocation' => $publocation,
+        ],
+        'ar' => [
+          'title' => $doc->ss_ar_title_long,  // ok
+          'identifier' => $doc->ss_book_identifier,  // ok
+          'path' => "book/{$doc->ss_book_identifier}/1?lang=ar",  // ok
+          'noid' => $doc->ss_noid,  // ok
+          'handle' => $doc->ss_handle,  // ok
+          'manifest' => $doc->ss_manifest,  // ok
+          'subjects' => [], // we do not display subjects in ar?
+          'pubdate' => $pubdate,  // ok
+          'pdf' => [
+            'hi' => $pdf_hi,
+            'lo' => $pdf_lo,
+          ],
+          'authors' => $authors_ar,  // ok
+          'partners' => $partners_ar,
+          'topics' => $topics,
+          'publishers' => $publishers,
+          'provider' => $providers,
+          'publocation' => $ar_publocation,
+        ],
+      ];
 
-    //   $documents[] = [
-    //     'en' => [
-    //       'title' => $doc->ss_title_long,
-    //       'identifier' => $doc->ss_book_identifier,
-    //       'path' => "book/{$doc->ss_book_identifier}/1",
-    //       'noid' => $doc->ss_noid,
-    //       'handle' => $doc->ss_handle,
-    //       'manifest' => $doc->ss_manifest,
-    //       'subjects' => $subjects,
-    //       'pubdate' => $pubdate,
-    //       'pdf' => [
-    //         'hi' => $pdf_hi,
-    //         'lo' => $pdf_lo,
-    //       ],
-    //       'authors' => $authors,
-    //       'partners' => $partners,
-    //       'topics' => $topics,
-    //       'publishers' => $publishers,
-    //       'provider' => $providers,
-    //       'publocation' => $publocation,
-    //     ],
-    //     'ar' => [
-    //       'title' => $doc->ss_ar_title_long,  // ok
-    //       'identifier' => $doc->ss_book_identifier,  // ok
-    //       'path' => "book/{$doc->ss_book_identifier}/1?lang=ar",  // ok
-    //       'noid' => $doc->ss_noid,  // ok
-    //       'handle' => $doc->ss_handle,  // ok
-    //       'manifest' => $doc->ss_manifest,  // ok
-    //       'subjects' => [], // we do not display subjects in ar?
-    //       'pubdate' => $pubdate,  // ok
-    //       'pdf' => [
-    //         'hi' => $pdf_hi,
-    //         'lo' => $pdf_lo,
-    //       ],
-    //       'authors' => $authors_ar,  // ok
-    //       'partners' => $partners_ar,
-    //       'topics' => $topics,
-    //       'publishers' => $publishers,
-    //       'provider' => $providers,
-    //       'publocation' => $ar_publocation,
-    //     ],
-    //   ];
-    // }
-
-    // return [
-    //   'documents' => $documents,
-    //   'total' => $total,
-    //   'rows' => $rows,
-    //   'page' => ($start / $rows) + 1,
-    // ];
-    return [
-      'documents' => [],
-      'total' => "??",
-      'rows' => $rows,
-      'page' => ($rowStart / $rows) + 1,
-    ];
-
-    // return [
-    //   'documents' => [],
-    //   'scopeIs' => $scopeIs,
-    //   'sortBy' => $sortBy,
-    //   'query' => $query,
-    // ];
-  }
-
-  function compareQueryToUrl(Client $client, Query $query, string $oldUrl)
-  {
-    // 1. Get the RequestBuilder for this specific query type
-    $requestBuilder = $client->getAdapter()->getRequestBuilder($query);
-    $request = $requestBuilder->build($query);
-
-    // 2. Extract the parameters from the Solarium query object
-    $newParams = $request->getParams();
-
-    // 3. Parse the parameters from your old URL
-    $urlParts = parse_url($oldUrl);
-    $oldParams = [];
-    if (isset($urlParts['query'])) {
-      parse_str($urlParts['query'], $oldParams);
+      // stop iteration on first loop
+      break;
     }
 
-    // 4. Find the differences
-    $added = array_diff_assoc($newParams, $oldParams);
-    $removed = array_diff_assoc($oldParams, $newParams);
-
     return [
-      'is_equal' => ($newParams == $oldParams),
-      'added_or_changed' => $added,
-      'removed_from_old' => $removed,
-      'full_new_params'  => $newParams
+      'documents' => $documents,
+      'total' => $total,
+      'rows' => $rows,
+      'page' => ($rowStart / $rows) + 1,
     ];
   }
 }
