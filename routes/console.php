1795965d84e0d610d64092bb8fe4b5042f36695f
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-01-26 08:59:07 -0800

    Add named argument function calling

--- routes/console.php
+++ routes/console.php
@@ -13,7 +13,15 @@ Artisan::command('inspire', function () {
  */
 // Artisan::command('solr:search {query="*:*"} {scopeIs="matches"} {sortBy="asc"} {rowStart} {rows}', function (SolrService $solrService, string $query, string $scopeIs, string $sortBy, int $rowStart, int $rows) {
 Artisan::command('solr:search {query="*:*"} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {rowStart=0} {rows=10}', function (string $query, string $scopeIs, string $sortField, string $sortDir, int $rowStart, int $rows, SolrService $solrService) {
-  $results = $solrService->search2($query, $scopeIs, $sortField, $sortDir, $rowStart, $rows);
+  $results = $solrService->search2(
+    fieldSelect: 'q', // same as scopeIs??
+    scopeIs: $scopeIs,
+    sortField: $sortField,
+    sortDir: $sortDir,
+    rowStart: $rowStart,
+    rows: $rows,
+    query: $query,
+  );
 
   dump($results);
   // $this->table(['Results'], $results);
