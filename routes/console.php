5c7a7b01c25a957f67c691c17f628f5f6bea74e1
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-01-27 14:06:12 -0800

    Switch over to search method

--- routes/console.php
+++ routes/console.php
@@ -13,7 +13,7 @@ Artisan::command('inspire', function () {
  */
 // Artisan::command('solr:search {query="*:*"} {scopeIs="matches"} {sortBy="asc"} {rowStart} {rows}', function (SolrService $solrService, string $query, string $scopeIs, string $sortBy, int $rowStart, int $rows) {
 Artisan::command('solr:search {query} {fieldSelect} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {rowStart=0} {rows=10}', function (string $query, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $rowStart, int $rows, SolrService $solrService) {
-  $results = $solrService->search2(
+  $results = $solrService->search(
     fieldSelect: $fieldSelect, // same as scopeIs??
     scopeIs: $scopeIs,
     sortField: $sortField,
