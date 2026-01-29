cb533994c66d9895536532da69dfaf3edd603ee9
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-01-29 12:01:05 -0800

    Remove rowStart for start

--- routes/console.php
+++ routes/console.php
@@ -11,19 +11,16 @@ Artisan::command('inspire', function () {
 /**
  * artisan command used to trigger the solr service without testing or calling the whole app
  */
-// Artisan::command('solr:search {query="*:*"} {scopeIs="matches"} {sortBy="asc"} {rowStart} {rows}', function (SolrService $solrService, string $query, string $scopeIs, string $sortBy, int $rowStart, int $rows) {
-Artisan::command('solr:search {query} {fieldSelect} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {rowStart=0} {rows=10}', function (string $query, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $rowStart, int $rows, SolrService $solrService) {
+Artisan::command('solr:search {query} {fieldSelect} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {start=0} {rows=10}', function (string $query, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $start, int $rows, SolrService $solrService) {
   $results = $solrService->search(
     fieldSelect: $fieldSelect, // same as scopeIs??
     scopeIs: $scopeIs,
     sortField: $sortField,
     sortDir: $sortDir,
-    rowStart: $rowStart,
+    start: $start,
     rows: $rows,
     query: $query,
   );
 
   dump($results);
-  // $this->table(['Results'], $results);
-  // dump($query);
 })->purpose('Run basic search through the search service');
