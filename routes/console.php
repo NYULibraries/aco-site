1524951c6911d363e82a29e18d28117398653fae
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-02-03 07:43:47 -0800

    Refactor function to use optional parameters, cast to int the numbers from cli

--- routes/console.php
+++ routes/console.php
@@ -12,16 +12,26 @@ Artisan::command('inspire', function () {
  * artisan command used to trigger the solr service without testing or calling the whole app
  */
 Artisan::command(
-  'solr:search {searchString="*:*"} {fieldSelect="q"} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {start=0} {rows=10}',
-  function (string $searchString, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $start, int $rows, SolrService $solrService) {
+  // 'solr:search {searchString="*:*"} {fieldSelect="q"} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {start=0} {rows=10}',
+  'solr:search
+    {searchString="*"}
+    {--F|field-select=q}
+    {--S|scope=matches}
+    {--sort-field=score}
+    {--sort-dir=desc}
+    {--start=0}
+    {--rows=10}',
+  //
+  // function (string $searchString, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $start, int $rows, SolrService $solrService) {
+  function (SolrService $solrService) {
     $results = $solrService->search(
-      fieldSelect: $fieldSelect, // same as scopeIs??
-      scopeIs: $scopeIs,
-      sortField: $sortField,
-      sortDir: $sortDir,
-      start: $start,
-      rows: $rows,
-      searchString: $searchString,
+      fieldSelect: $this->option('field-select'),
+      scopeIs: $this->option('scope'),
+      sortField: $this->option('sort-field'),
+      sortDir: $this->option('sort-dir'),
+      start: (int) $this->option('start'),
+      rows: (int) $this->option('rows'),
+      searchString: $this->argument('searchString'),
     );
 
     dump($results);
