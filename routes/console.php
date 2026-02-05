c054c712df882ff8110b6415b5f09546e0ee7a6b
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-02-05 11:59:32 -0800

    Add logger

--- routes/console.php
+++ routes/console.php
@@ -3,16 +3,16 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Services\SolrService;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
   $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 /**
- * artisan command used to trigger the solr service without testing or calling the whole app
+ * artisan command used to call the SolrService
  */
 Artisan::command(
-  // 'solr:search {searchString="*:*"} {fieldSelect="q"} {scopeIs="matches"} {sortField="score"} {sortDir="asc"} {start=0} {rows=10}',
   'solr:search
     {searchString="*"}
     {--F|field-select=q}
@@ -21,17 +21,24 @@ Artisan::command(
     {--sort-dir=desc}
     {--start=0}
     {--rows=10}',
-  //
-  // function (string $searchString, string $fieldSelect, string $scopeIs, string $sortField, string $sortDir, int $start, int $rows, SolrService $solrService) {
   function (SolrService $solrService) {
+    Log::info("Artisan Command", [
+      "fieldSelect" => $this->option('field-select'),
+      "scopeIs" => $this->option('scope'),
+      "searchString" => $this->argument('searchString'),
+      "sortField" => $this->option('sort-field'),
+      "sortDir" => $this->option('sort-dir'),
+      "start" => (int) $this->option('start'),
+      "rows" => (int) $this->option('rows'),
+    ]);
     $results = $solrService->search(
       fieldSelect: $this->option('field-select'),
       scopeIs: $this->option('scope'),
+      searchString: $this->argument('searchString'),
       sortField: $this->option('sort-field'),
       sortDir: $this->option('sort-dir'),
       start: (int) $this->option('start'),
       rows: (int) $this->option('rows'),
-      searchString: $this->argument('searchString'),
     );
 
     dump($results);
