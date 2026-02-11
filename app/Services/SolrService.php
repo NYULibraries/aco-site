30e74bfc3cc1453bb2f1dba63ef2ed9240afbd16
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2026-02-11 08:52:45 -0800

    Use new resources in tranform metho

--- app/Services/SolrService.php
+++ app/Services/SolrService.php
@@ -5,6 +5,9 @@ namespace App\Services;
 use Solarium\Client;
 use Solarium\QueryType\Select\Query\Query;
 use Solarium\QueryType\Select\Result\Result as SolariumResult;
+use App\Http\Resources\SolrCollectionResource;
+use App\Http\Resources\SolrDocumentResource;
+
 
 class SolrService
 {
@@ -194,188 +197,17 @@ class SolrService
 
   /**
    * Transforms Solarium Result into usable data by our application
+   * uses Laravel Resources
    * @param $resultset - Solarium result from executing a query
    * @return array - final transformed data
    */
   public function transformData(SolariumResult $resultset): array
   {
-
-    $total = $resultset->getNumFound();
-    $docs = collect($resultset->getDocuments());
-
-    $transformedDocs = $docs->map(function ($doc) {
-      $publocation = [];
-      if (isset($doc->ss_publocation)) {
-        $publocation[] = [
-          'label' => $doc->ss_publocation,
-          'path' => "search/?provider={$doc->ss_publocation}",
-        ];
-      }
-
-      $ar_publocation = [];
-      if (isset($doc->ss_ar_publocation)) {
-        $ar_publocation[] = [
-          'label' => $doc->ss_ar_publocation,
-          'path' => "search/?provider={$doc->ss_ar_publocation}",
-        ];
-      }
-
-      $providers = [];
-      if (isset($doc->sm_provider_label)) {
-        foreach ($doc->sm_provider_label as $provider) {
-          $providers[] = [
-            'label' => $provider,
-            'path' => "search/?provider={$provider}",
-          ];
-        }
-      }
-
-      $publishers = [];
-      if (isset($doc->sm_publisher)) {
-        foreach ($doc->sm_publisher as $publisher) {
-          $publishers[] = [
-            'label' => $publisher,
-            'path' => "search/?publisher={$publisher}",
-          ];
-        }
-      }
-
-      $topics = [];
-      if (isset($doc->sm_field_topic)) {
-        foreach ($doc->sm_field_topic as $topic) {
-          $topics[] = [
-            'label' => $topic,
-            'path' => "search?category={$topic}&scope=matches",
-          ];
-        }
-      }
-
-      $subjects = [];
-      if (isset($doc->zm_subject)) {
-        foreach ($doc->zm_subject as $subject) {
-          $subject = json_decode($subject);
-          $subject->path = "search?subject={$subject->name}";
-          $subjects[] = $subject;
-        }
-      }
-
-      $partners_map = [
-        'Arabic collections online' => 'المجموعات العربية على الانترنت',
-        'New York University Libraries' => 'مكتبات جامعة نيويورك',
-        'Princeton University Libraries' => 'مكتبات جامعة برينستون',
-        'Cornell University Libraries' => 'مكتبات جامعة كورنيل',
-        'Columbia University Libraries' => 'مكتبات جامعة كولومبيا',
-        'American University of Beirut' => 'الجامعة الاميركية في بيروت',
-        'American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
-        'The American University in Cairo' => 'الجامعة الاميركية بالقاهرة',
-        'United Arab Emirates National Archives' => 'الامارات العربية المتحدة - الارشيف الوطني',
-      ];
-
-      $partners = [];
-      $partners_ar = [];
-      if (isset($doc->zm_partner)) {
-        foreach ($doc->zm_partner as $partner) {
-          $partner = json_decode($partner);
-          $partner->path = "search?partner={$partner->name}";
-          $partners[] = $partner;
-          if (isset($partners_map[$partner->name])) {
-            $partners_ar[] = [
-              'label' => $partners_map[$partner->name],
-              'path' => "search?partner={$partner->name}",
-            ];
-          }
-        }
-      }
-
-      $authors = [];
-      if (isset($doc->sm_author)) {
-        foreach ($doc->sm_author as $author) {
-          $authors[] = [
-            'label' => $author,
-            'path' => "search?subject={$author}",
-          ];
-        }
-      }
-
-      $authors_ar = [];
-      if (isset($doc->sm_ar_author)) {
-        foreach ($doc->sm_ar_author as $author) {
-          $authors_ar[] = [
-            'label' => $author,
-            'path' => "search?subject={$author}",
-          ];
-        }
-      }
-
-      $pdf_hi = [];
-      if (isset($doc->zm_pdf_hi) && isset($doc->zm_pdf_hi[0])) {
-        $pdf_hi = json_decode($doc->zm_pdf_hi[0]);
-      }
-
-      $pdf_lo = [];
-      if (isset($doc->zm_pdf_lo) && isset($doc->zm_pdf_lo[0])) {
-        $pdf_lo = json_decode($doc->zm_pdf_lo[0]);
-      }
-
-      $pubdate = 'n.d.';
-      if (isset($doc->ss_pubdate) && isset($doc->ss_pubdate)) {
-        $pubdate = $doc->ss_pubdate;
-      }
-
-      return [
-        'en' => [
-          'title' => $doc->ss_title_long,
-          'identifier' => $doc->ss_book_identifier,
-          'path' => "book/{$doc->ss_book_identifier}/1",
-          'noid' => $doc->ss_noid,
-          'handle' => $doc->ss_handle,
-          'manifest' => $doc->ss_manifest,
-          'subjects' => $subjects,
-          'pubdate' => $pubdate,
-          'pdf' => [
-            'hi' => $pdf_hi,
-            'lo' => $pdf_lo,
-          ],
-          'authors' => $authors,
-          'partners' => $partners,
-          'topics' => $topics,
-          'publishers' => $publishers,
-          'provider' => $providers,
-          'publocation' => $publocation,
-        ],
-        'ar' => [
-          'title' => $doc->ss_ar_title_long,  // ok
-          'identifier' => $doc->ss_book_identifier,  // ok
-          'path' => "book/{$doc->ss_book_identifier}/1?lang=ar",  // ok
-          'noid' => $doc->ss_noid,  // ok
-          'handle' => $doc->ss_handle,  // ok
-          'manifest' => $doc->ss_manifest,  // ok
-          'subjects' => [], // we do not display subjects in ar?
-          'pubdate' => $pubdate,  // ok
-          'pdf' => [
-            'hi' => $pdf_hi,
-            'lo' => $pdf_lo,
-          ],
-          'authors' => $authors_ar,  // ok
-          'partners' => $partners_ar,
-          'topics' => $topics,
-          'publishers' => $publishers,
-          'provider' => $providers,
-          'publocation' => $ar_publocation,
-        ],
-      ];
-    });
-
-    $rows2 = $resultset->getQuery()->getOption('rows');
-    $start2 = $resultset->getQuery()->getOption('start');
-
-    return [
-      // note: not transforming toArray since this is to be used within the Blade templates
-      'documents' => $transformedDocs,
-      'total' => $total,
-      'rows' => $rows2,
-      'page' => ($start2 / $rows2) + 1,
-    ];
+    $rawDocs = collect($resultset->getDocuments());
+    $resourceDocs = SolrDocumentResource::collection($rawDocs);
+    $collection = new SolrCollectionResource($resourceDocs, $resultset);
+    // resolving as plain array for blade templates
+    return $collection->resolve();
   }
 
   /**
