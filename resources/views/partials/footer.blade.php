a244da035209e99a3af247d770e647f3c0f37002
Alberto Ortiz Flores <aortiz@nyu.edu>
2026-02-18 21:45:43 -0500

    Ignore ddev - Migth want to add this to the project another time

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,42 +12,42 @@
     <div class="logo-hold">
         <div class="logo nyu">
             <a href="http://www.nyu.edu/">
-                <img src="{{ Vite::asset('resources/images/logos/nyu_short_color.png') }}" title="New York University"
+                <img src="{{ asset('images/logos/nyu_short_color.png') }}" title="New York University"
                     alt="New York University">
             </a>
         </div>
         <div class="logo nyuad">
-            <a href="http://www.nyu.edu/"><img src="{{ Vite::asset('resources/images/logos/nyuad.png') }}"
+            <a href="http://www.nyu.edu/"><img src="{{ asset('images/logos/nyuad.png') }}"
                     title="New York University Abu Dhabi" alt="New York University Abu Dhabi"></a>
         </div>
         <div class="logo columbia">
-            <a href="http://www.columbia.edu/"><img src="{{ Vite::asset('resources/images/logos/columbia.png') }}"
+            <a href="http://www.columbia.edu/"><img src="{{ asset('images/logos/columbia.png') }}"
                     title="Columbia University in the City of New York"
                     alt="Columbia University in the City of New York"></a>
         </div>
         <div class="logo cornell">
-            <a href="http://www.cornell.edu/"><img src="{{ Vite::asset('resources/images/logos/cornell.png') }}"
+            <a href="http://www.cornell.edu/"><img src="{{ asset('images/logos/cornell.png') }}"
                     title="Cornell University" alt="Cornell University"></a>
         </div>
         <div class="logo princeton">
-            <a href="https://www.princeton.edu/"><img src="{{ Vite::asset('resources/images/logos/princeton.png') }}"
+            <a href="https://www.princeton.edu/"><img src="{{ asset('images/logos/princeton.png') }}"
                     title="Princeton University" alt="Princeton University"></a>
         </div>
         <div class="logo aub">
-            <a href="http://www.aub.edu.lb/"><img src="{{ Vite::asset('resources/images/logos/aub.png') }}"
+            <a href="http://www.aub.edu.lb/"><img src="{{ asset('images/logos/aub.png') }}"
                     title="American University of Beirut" alt="American University of Beirut"></a>
         </div>
         <div class="logo auc">
-            <a href="http://www.aucegypt.edu/"><img src="{{ Vite::asset('resources/images/logos/auc.png') }}"
+            <a href="http://www.aucegypt.edu/"><img src="{{ asset('images/logos/auc.png') }}"
                     title="The American University in Cairo" alt="The American University in Cairo"></a>
         </div>
         <div class="logo uae">
-            <a href="https://www.nla.ae/"><img src="{{ Vite::asset('resources/images/logos/uae.png') }}"
+            <a href="https://www.nla.ae/"><img src="{{ asset('images/logos/uae.png') }}"
                     title="United Arab Emirates National Library and Archives"
                     alt="United Arab Emirates National Library and Archives"></a>
         </div>
         <div class="logo qnl">
-            <a href="https://www.qnl.qa/"><img src="{{ Vite::asset('resources/images/logos/qnl.png') }}"
+            <a href="https://www.qnl.qa/"><img src="{{ asset('images/logos/qnl.png') }}"
                     title="Qatar National Library" alt="Qatar National Library"></a>
         </div>
     </div>
