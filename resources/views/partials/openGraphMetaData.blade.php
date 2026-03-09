ab358e83151463368b59639671a0718972065a77
Damon Chu <dc3800@nyu.edu>
2026-03-09 10:54:38 -0400

    og tags

--- resources/views/partials/openGraphMetaData.blade.php
+++ resources/views/partials/openGraphMetaData.blade.php
@@ -1,1 +1,11 @@
-
+<meta property="og:site_name" content="Arabic Collections Online">
+<meta property="og:title" content="{{ !empty($pagetitle) ? $pagetitle : 'Arabic Collections Online' }}">
+<meta property="og:description"
+    content="Arabic Collections Online is a publicly available digital library of public domain Arabic language content. Funded by New York University Abu Dhabi, this project aims to expose 23,000 volumes from NYU and partner institutions over a period of five years.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://aco.dlib.nyu.edu/">
+<meta property="og:image:secure_url" content="https://aco.dlib.nyu.edu/images/ACO_opengraph_thumb1200.jpg">
+<meta property="og:image" content="https://aco.dlib.nyu.edu/images/ACO_opengraph_thumb1200.jpg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:image:type" content="image/png">
