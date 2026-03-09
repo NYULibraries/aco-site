3b601b7bc038df6bb33d0604700654e5c3f03a86
Damon Chu <dc3800@nyu.edu>
2026-03-09 12:56:07 -0400

    added config file for og tags

---
+++ config/og.php
@@ -0,0 +1,20 @@
+<?php
+
+// page title is set in the controller
+// app url is set in the config/app.php
+return [
+
+    'site_name' => 'Arabic Collections Online',
+
+    'description' => 'Arabic Collections Online is a publicly available digital library of public domain Arabic language content. Funded by New York University Abu Dhabi, this project aims to expose 23,000 volumes from NYU and partner institutions over a period of five years.',
+
+    // app url is appended to this image path using asset()
+    'image' => '/images/ACO_opengraph_thumb1200.jpg',
+
+    'image_width' => '1200',
+
+    'image_height' => '630',
+
+    'image_type' => 'image/png',
+
+];
